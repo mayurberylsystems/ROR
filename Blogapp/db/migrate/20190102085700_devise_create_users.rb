e680d75a61d9a2ea233825ed2a3cd75997014b38
mayursaxena <mayursaxena162@gmail.com>
2019-01-02 15:39:35 +0530

    2/01/19

--- Blogapp/db/migrate/20190102085700_devise_create_users.rb
+++ Blogapp/db/migrate/20190102085700_devise_create_users.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
-class DeviseCreateGem:installs < ActiveRecord::Migration[5.2]
+class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
-    create_table :gem:installs do |t|
+   # create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      #t.string :email,              null: false, default: ""
+      #t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      #t.string   :reset_password_token
+      #t.datetime :reset_password_sent_at
 
       ## Rememberable
-      t.datetime :remember_created_at
+      #t.datetime :remember_created_at
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
@@ -33,12 +33,12 @@ class DeviseCreateGem:installs < ActiveRecord::Migration[5.2]
       # t.datetime :locked_at
 
 
-      t.timestamps null: false
+      #t.timestamps null: false
     end
 
-    add_index :gem:installs, :email,                unique: true
-    add_index :gem:installs, :reset_password_token, unique: true
-    # add_index :gem:installs, :confirmation_token,   unique: true
-    # add_index :gem:installs, :unlock_token,         unique: true
+    #add_index :users, :email,                unique: true
+    #add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
   end
 end
