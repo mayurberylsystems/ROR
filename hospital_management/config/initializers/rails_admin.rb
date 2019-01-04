b6ddd6967d9ec67735b7345296af5b3d0fda3dd0
mayursaxena <mayursaxena162@gmail.com>
2019-01-04 17:01:17 +0530

    admin gem

--- hospital_management/config/initializers/rails_admin.rb
+++ hospital_management/config/initializers/rails_admin.rb
@@ -1,12 +1,18 @@
 RailsAdmin.config do |config|
+#    config.authorize_with do
+#     redirect_to new_user_session_path unless current_user.is_admin?
+#   end
+#    config.authorize_with :devise
+# end
 
   ### Popular gems integration
 
   ## == Devise ==
   # config.authenticate_with do
-  #   warden.authenticate! scope: :user
+  #    warden.authenticate! scope: :user
+  #  end
+  #  config.current_user_method(&:current_user)
   # end
-  # config.current_user_method(&:current_user)
 
   ## == Cancan ==
   # config.authorize_with :cancan
