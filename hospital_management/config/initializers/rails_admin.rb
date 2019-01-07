8ed944dcfa9ca83d47928e5d94b52f083dfe846d
mayursaxena <mayursaxena162@gmail.com>
2019-01-07 17:48:10 +0530

    07/01/19

--- hospital_management/config/initializers/rails_admin.rb
+++ hospital_management/config/initializers/rails_admin.rb
@@ -1,4 +1,12 @@
 RailsAdmin.config do |config|
+  config.authorize_with do
+    authenticate_or_request_with_http_basic('Site Message') do |username, password|
+      username == 'mayur.berylsystem' && password == 'Mayur@123'
+    end
+  end
+
+   config.main_app_name { ['My App', 'Admin'] }
+#end  
 
  
 
