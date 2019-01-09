5d4a78a8747449c3c414ae03b2789fc4f925e3ca
mayursaxena <mayursaxena162@gmail.com>
2019-01-09 16:07:10 +0530

    Requests to perform CRUD operation on Posts stored in database

--- RestApi/app/controllers/api/v1/posts_controller.rb
+++ RestApi/app/controllers/api/v1/posts_controller.rb
@@ -5,6 +5,33 @@ module Api
 				posts = Post.all
 				render json: {status: 'SUCCESS', message: 'Posts by Admin', data:posts}, status: :ok
 			end
+
+			def show
+				post = Post.find(params[:id])
+				render json: {status: 'SUCCESS', message: 'Posts by Admin', data:post}, status: :ok
+			end
+
+			def create
+				post = Post.new(post_params)
+
+				if post.save
+					render json: {status: 'SUCCESS', message: 'Saved Post by Admin', data:post}, status: :ok
+				else
+					render json: {status: 'ERROR', message: 'Post Not Saved Yet', data:post.errors}, status: :unprocessable_entity
+				end
+			end 
+
+			def destroy
+				post = Post.find(params[:id])
+				post.destroy
+				render json: {status: 'SUCCESS', message: 'Deleted Post', data:post}, status: :ok
+			end
+
+			private
+
+			def post_params
+				params.permit(:title, :content)
+			end
 		end
 	end
 end
