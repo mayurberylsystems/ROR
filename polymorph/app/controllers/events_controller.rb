a0bdd89c245e6d2bbfe58625b0c6b2889b61cab2
Shantanu <shantanu.rana@berylsystems.com>
2019-01-11 04:53:00 +0530

    created event mvc section

--- polymorph/app/controllers/events_controller.rb
+++ polymorph/app/controllers/events_controller.rb
@@ -1,24 +1,42 @@
 class EventsController < ApplicationController
 	
 	def index
+		@events = Event.all
 	end
 	
 	def new
 	end
 	
 	def create
+		@event = Event.new(event_params)
+		@event.save
+		redirect_to events_path
 	end
 	
 	def show
+		@event = Event.find(params[:id])
 	end
 	
 	def edit
+		@event = Event.find(params[:id])
 	end
 	
 	def update
+		@event = Event.find(params[:id])
+		if @event.update(event_params)
+			redirect_to events_path
+		else 
+			render 'edit'
+		end
 	end
 
 	def destroy
+		event = Event.find(params[:id])
+		event.destroy
+		redirect_to events_path
 	end
 
+	def event_params
+		params.require(:event).permit(:name,:location)
+	end
 end
--- polymorph/app/controllers/users_controller.rb
+++ polymorph/app/controllers/users_controller.rb
@@ -1,24 +1,41 @@
 class UsersController < ApplicationController
 	
 	def index
+		@events = Event.all
 	end
 	
 	def new
 	end
 	
 	def create
+		@event = Event.new(event_params)
+		@event.save
+		redirect_to events_path
 	end
 	
 	def show
+		@event = Event.find(params[:id])
 	end
 	
 	def edit
+		@event = Event.find(params[:id])
 	end
 	
 	def update
+		@event = Event.find(params[:id])
+		if @event.update(event_params)
+			redirect_to events_path
+		else 
+			render 'edit'
+		end
 	end
 
 	def destroy
+		event = Event.find(params[:id])
+		event.destroy
+		redirect_to events_path
 	end
 
-end
+	def event_params
+		params.require(:event).permit(:name,:location)
+	endend
