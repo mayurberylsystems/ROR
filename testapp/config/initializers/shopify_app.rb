16ce32ac01961a152af38c33fb4de5a65615b09e
Shantanu <shantanu.rana@berylsystems.com>
2019-01-15 11:42:42 +0530

    test app for shopify

---
+++ testapp/config/initializers/shopify_app.rb
@@ -0,0 +1,7 @@
+ShopifyApp.configure do |config|
+  config.application_name = "Etah store"
+  config.api_key = "b7f709fb9dae329cae1a155432525ac3"
+  config.secret = "aad07d648da3920773e7b68f78189879"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
