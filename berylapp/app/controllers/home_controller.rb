3e215b01316668237d72d696370f44518dc39f20
mayursaxena <mayursaxena162@gmail.com>
2019-01-16 11:41:12 +0530

    sync products from shopify

--- berylapp/app/controllers/home_controller.rb
+++ berylapp/app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
+  	@products.each do |x|
+  		@details = Product.create(title: x.title, description: x.body_html)
+  	end
   end
+
+  def productsync
+  	@products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
+  end
+
 end
--- mayurtestapp/config/initializers/shopify_app.rb
+++ mayurtestapp/config/initializers/shopify_app.rb
@@ -2,7 +2,7 @@ ShopifyApp.configure do |config|
   config.application_name = "Online Test Store"
   config.api_key = "c5d10dc69fba9ff422d85b0d3ecbfab7"
   config.secret = "26ce9c076829503f215c61288e43c164"
-  config.scope = "read_products" # Consult this page for more scope options:
+  config.scope = "read_orders, read_products" # Consult this page for more scope options:
                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.embedded_app = true
   config.after_authenticate_job = false
