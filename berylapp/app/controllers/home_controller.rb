c14a1dd349ba9a93e5ba3c25e93d08bcf429d037
mayursaxena <mayursaxena162@gmail.com>
2019-01-21 11:07:48 +0530

    carrier wave

--- berylapp/app/controllers/home_controller.rb
+++ berylapp/app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ShopifyApp::AuthenticatedController
   
   def index
+  
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @products.each do |x|
-      debugger
       product = Shopproduct.create(shop_id: 1, title: x.title, description: x.body_html, price: x.variants.first.price)
       image = Shop.first.shopproducts.first.shopproductimages.create(remote_image_url: x.images.first.src)
     end
