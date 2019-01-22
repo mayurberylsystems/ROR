46ee78a6975a0e3d9de61bebf6dd86a424be78a2
mayursaxena <mayursaxena162@gmail.com>
2019-01-22 17:17:34 +0530

    few changes in query for images

--- berylapp/app/controllers/home_controller.rb
+++ berylapp/app/controllers/home_controller.rb
@@ -5,7 +5,9 @@ class HomeController < ShopifyApp::AuthenticatedController
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @products.each do |x|
       product = Shopproduct.create(shop_id: 1, title: x.title, description: x.body_html, price: x.variants.first.price)
-      image = Shop.first.shopproducts.first.shopproductimages.create(remote_image_url: x.images.first.src)
+      x.images.each do |y|
+      	image = Shopproductimage.create(shopproduct_id: product.id, remote_image_url: y.src)
+      end
     end
   end
 end
