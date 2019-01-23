054fd95cea697f51a4a83fc70226b51ded1971d6
mayursaxena <mayursaxena162@gmail.com>
2019-01-23 16:21:52 +0530

    integration of a rails application with quickbook_ruby

--- appquickbook/config/routes.rb
+++ appquickbook/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'quickbooks#index'
+  get 'quickbooks/authenticate'
+  get 'quickbooks/oauth_callback'
 end
